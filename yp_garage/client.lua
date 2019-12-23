4838eafa5d082ab4d81c2fec7988fa8b7304e1cb
frontoge <frontoge@users.noreply.github.com>
2019-12-23 14:17:51 -0500

    added custom garage script

---
+++ yp_garage/client.lua
@@ -0,0 +1,331 @@
+--[[ Copyright (C) Matthew Widenhouse - All Rights Reserved
+ * Unauthorized copying of this file, without written consent from the owner, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Matthew Widenhouse <widenhousematthew@gmail.com>, September 2019
+]]--
+
+--ESX Init
+ESX = nil
+
+--Mod enum
+local vehMods =
+{
+	"modSpoilers",
+	"modFrontBumper",
+	"modRearBumper",
+	"modSideSkirt",
+	"modExhaust",
+	"modFrame",
+	"modGrille",
+	"modHood",
+	"modFender",
+	"modRightFender",
+	"modRoof",
+	"modEngine",
+	"modBrakes",
+	"modTransmisson",
+	"Horns",
+	"modSuspension",
+	"modArmor",
+	"UNK17",
+	'modTurbo',
+	'UNK19',
+	'modSmokeEnabled',
+	'UNK21',
+	'modXenon',
+	"modFrontWheels",
+	"modBackWheels",
+	"modPlateHolder",
+	"modVanityPlate",
+	"modTrimA",
+	"modOrnaments",
+	'modDashboard',
+	"modDial",
+	'modDoorSpeaker',
+	'modSeats',
+	"modSteeringWheel",
+	"modShifterLeavers",
+	'modAPlate',
+	'modSpeakers',
+	'modTrunk',
+	"modHydrolic",
+	'modEngineBlock',
+	'modAirFilter',
+	'modStruts',
+	'modArchCover',
+	'modAerials',
+	'modTrimB',
+	'modTank',
+	'modWindows',
+	'UNK47',
+	"modLivery"
+}
+
+local garageBlips = {}
+
+function modelVehicle(vehicle, list, plate)
+	local color1 = list.color1
+	local color2 = list.color2
+	local tint = list.windowTint
+	SetVehicleNumberPlateText(vehicle, plate)
+
+	SetVehicleColours(vehicle, color1, color2)
+
+	if tint ~= -1 then
+		SetVehicleWindowTint(vehicle, tint)
+	end
+
+	--Add Mods 
+	for i, v in ipairs(vehMods) do
+		local value = list[v]
+		if value == 'true' then
+			value = true
+		elseif value == 'false' then
+			value = false
+		else
+			value = tonumber(value)
+		end
+		SetVehicleMod(vehicle, i, value) --May need to be i-1 if there are issues
+	end
+end
+
+function getVehicleData(vehicle)
+	local data = {}
+	local c1, c2 = GetVehicleColours(vehicle)
+	data['color1'] = c1
+	data['color2'] = c2
+	data['windowTint'] = GetVehicleWindowTint(vehicle)
+	data['model'] = GetEntityModel(vehicle)
+	data['engineHealth'] = GetVehicleEngineHealth(vehicle)
+	data['bodyHealth'] = GetVehicleBodyHealth(vehicle)
+	data['fuelTankHealth'] = GetVehiclePetrolTankHealth(vehicle)
+	data['plate'] = GetVehicleNumberPlateText(vehicle)
+
+
+	for i, v in ipairs(vehMods) do
+		if string.find(v, 'UNK') == nil then
+			local temp = GetVehicleMod(vehicle, i)
+			data[v] = temp
+		end
+	end
+
+	return data
+
+end
+
+RegisterNetEvent('yp_garage:deleteCar')
+AddEventHandler('yp_garage:deleteCar', function()
+	local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1))
+	exports['yp_base']:deleteVehicle(vehicle)
+end)
+
+RegisterNetEvent('yp_garage:openVehicleMenu')
+AddEventHandler('yp_garage:openVehicleMenu', function(data)
+	local elements = data
+	local vehicle = nil
+	Citizen.CreateThread(function()
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'pull_out', {
+			title = 'Vehicles',
+			align = 'bottom-right',
+			elements = elements},
+			function(data, menu)
+				menu.close()
+				local action = data.current.value
+				if action ~= 'not_stored' then
+					local props = json.decode(action)
+					local model = props.model
+					local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1)))
+					RequestModel(model)
+					while not HasModelLoaded(model) do
+						Citizen.Wait(0)
+					end
+					vehicle = CreateVehicle(model, x, y, z, 165.0, true, true)
+					TaskWarpPedIntoVehicle(GetPlayerPed(-1), vehicle, -1)
+					TriggerServerEvent('yp_garage:pullVehicle', data.current.plate)
+					modelVehicle(vehicle, props, data.current.plate)
+					
+				else
+					exports['mythic_notify']:DoHudText('error', 'This vehicle is already outside')
+				end
+			end,
+			function(data, menu)
+				menu.close()
+			end)
+	end)
+end)
+
+RegisterNetEvent('yp_garage:openInsureMenu')
+AddEventHandler('yp_garage:openInsureMenu', function(data, garageName)
+	local elements = data
+
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'insure_menu', {
+		title = 'Vehicles',
+		align = 'bottom-right',
+		elements = elements},
+
+		function(data, menu)
+			local action = data.current
+			ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'manage_type', {
+				title = 'Vehicles',
+				align = 'bottom-right',
+				elements = 
+				{
+					{label = 'Manage', value = 'manage'},
+					{label = 'Rename', value = 'rename'}
+				}},
+
+				function(data2, menu2)
+					if data2.current.value == 'manage' then --Move/Insure Vehicles
+						if action.value == 'out' then
+							ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'insureveh', {
+								title = 'Pay ($' .. InsurePrice .. ') to insure?',
+								align = 'bottom-right',
+								elements = 
+								{
+									{label = 'Yes', value = 'y'},
+									{label = 'No', value = 'n'}
+								}},
+
+								function(data3, menu3)
+									local action2 = data3.current.value
+									if action2 == 'y' then
+										TriggerServerEvent('yp_garage:storeVehicle', action.plate, garageName, nil)
+										TriggerServerEvent('yp_base:payFee', InsurePrice)
+										menu2.close()
+										menu3.close()
+									else
+										menu2.close()
+										menu3.close()
+									end
+								end,
+								function(data3, menu3)
+
+								end)
+						else
+							if action.value ~= garageName then
+								ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'moveveh',{
+									title = 'Pay ($' .. MovePrice .. ') to move here?',
+									align = 'bottom-right',
+									elements =
+									{
+										{label = 'Yes', value = 'y'},
+										{label = 'No', value = 'n'}
+									}},
+									function(data3, menu3)
+										local action2 = data3.current.value
+										if action2 == 'y' then
+											TriggerServerEvent('yp_garage:storeVehicle', action.plate, garageName, nil)
+											TriggerServerEvent('yp_base:payFee', MovePrice)
+											menu2.close()
+											menu3.close()
+										else
+											menu2.close()
+											menu3.close()
+										end
+									end,
+									function(data3, menu3)
+
+									end)
+							else
+								exports['mythic_notify']:DoHudText('inform', 'This vehicle is already here.')
+							end
+						end
+					else
+						ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'rename_option', {title = 'New Name'},
+							function(data3, menu3)
+								TriggerServerEvent('yp_garage:renameVehicle', action.plate, data3.value)
+								menu2.close()
+								menu3.close()
+							end,
+							function(data3, menu3)
+								menu2.close()
+								menu3.close()
+							end)
+					end
+				end,
+				function(data2, menu2)
+					menu2.close()
+				end)
+
+			
+		end,
+		function(data, menu)
+			menu.close()
+		end)
+
+end)
+
+--Create Blips 
+Citizen.CreateThread(function()
+	for i, v in ipairs(Garages) do
+		garageBlips[i] = AddBlipForCoord(v.pick.x, v.pick.y, v.pick.z)
+		SetBlipSprite(garageBlips[i], 50)
+		SetBlipDisplay(garageBlips[i], 4)
+		SetBlipScale(garageBlips[i], 1.0)
+		SetBlipColour(garageBlips[i], 3)
+		SetBlipAsShortRange(garageBlips[i], true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString("Garage")
+		EndTextCommandSetBlipName(garageBlips[i])
+	end
+end)
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	while true do
+		local playerPed = GetPlayerPed(-1)
+		local x, y, z = table.unpack(GetEntityCoords(playerPed))
+
+		for i, v in ipairs(Garages) do
+			--Drop offs
+			if Vdist(x, y, z, v.drop.x, v.drop.y, v.drop.z) < 25 then
+				DrawMarker(1, v.drop.x, v.drop.y, v.drop.z-1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 1.0, 255, 0, 0, 100, false, false, 2, false, nil, nil, false)
+				if Vdist(x, y, z, v.drop.x, v.drop.y, v.drop.z) < 2 then
+					if IsPedInAnyVehicle(playerPed) then
+						local vehicle = GetVehiclePedIsIn(playerPed)
+						if GetPedInVehicleSeat(vehicle, -1) == playerPed then
+							exports['yp_base']:DisplayHelpText('Press ~INPUT_CONTEXT~ to put away this vehicle')
+							if IsControlJustPressed(0, 51) then
+								--Store Vehicle Data and check that the car is theirs first
+								--Or maybe not check tbh
+								local plateNum = GetVehicleNumberPlateText(vehicle)
+								TriggerServerEvent('yp_garage:storeVehicle', plateNum, v.name, getVehicleData(vehicle))
+							end
+						end
+					end
+				end
+			end
+
+			--Pickup
+			if Vdist(x, y, z, v.pick.x, v.pick.y, v.pick.z) < 25 then
+				DrawMarker(1, v.pick.x, v.pick.y, v.pick.z-1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 1.0, 0, 0, 255, 100, false, false, 2, false, nil, nil, false)
+				if Vdist(x, y, z, v.pick.x, v.pick.y, v.pick.z) < 2 then
+					exports['yp_base']:DisplayHelpText('Press ~INPUT_CONTEXT~ to grab a vehicle')
+					if IsControlJustPressed(0, 51) then
+						TriggerServerEvent('yp_garage:getVehicles', v.name)
+					end
+				end
+			end
+
+			--insure
+			if v.insure ~= nil then
+				if Vdist(x, y, z, v.insure.x, v.insure.y, v.insure.z) < 25 then
+					DrawMarker(21, v.insure.x, v.insure.y, v.insure.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.75, 0.75, 0.75, 255, 204, 0, 100, false, false, 2, true, nil, nil, false)
+					if Vdist(x, y, z, v.insure.x, v.insure.y, v.insure.z) < 1 then
+						exports['yp_base']:DisplayHelpText('Press ~INPUT_CONTEXT~ to manage vehicles')
+						if IsControlJustPressed(0, 51) then
+							TriggerServerEvent('yp_garage:getAllVehicles', v.name)
+						end
+					end
+				end
+			end
+
+		end
+
+		Citizen.Wait(0)
+	end
+end)
---
+++ yp_garage/server.lua
@@ -0,0 +1,132 @@
+--[[ Copyright (C) Matthew Widenhouse - All Rights Reserved
+ * Unauthorized copying of this file, without written consent from the owner, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Matthew Widenhouse <widenhousematthew@gmail.com>, September 2019
+]]--
+
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+MySQL.Async.execute("UPDATE owned_vehicles SET state = true WHERE state = false", {}, function() end)
+
+RegisterServerEvent('yp_garage:storeVehicle')
+AddEventHandler('yp_garage:storeVehicle', function(plateNum, name, vehData)
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
+	local valid = false
+	local fetching = true
+
+	Citizen.CreateThread(function()
+		MySQL.Async.fetchAll("SELECT * FROM owned_vehicles WHERE plate = @plate", {['@plate'] = plateNum},
+		function(results)
+			if results ~= nil and #results > 0 then
+				valid = true
+				fetching = false		
+			end
+
+			if valid then
+				if vehData ~= nil then
+					MySQL.Async.execute("UPDATE owned_vehicles SET state = true, garage_name = @name, vehicle = @vehicle WHERE plate = @plate", {['@plate'] = plateNum, ['@name'] = name, ['@vehicle'] = json.encode(vehData)},
+						function()
+							TriggerClientEvent('yp_garage:deleteCar', src)
+						end)
+				else
+					MySQL.Async.execute("UPDATE owned_vehicles SET state = true, garage_name = @name WHERE plate = @plate", {['@plate'] = plateNum, ['@name'] = name},
+						function()
+							
+						end)
+				end
+			else
+				TriggerClientEvent('mythic_notify:client:SendAlert', src, {type = 'error', text = 'This vehicle does not have a valid registration.', length = 2500})
+			end
+
+		end)
+		
+	end)
+end)
+
+RegisterServerEvent('yp_garage:getVehicles')
+AddEventHandler('yp_garage:getVehicles', function(name)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local src = source
+	local data = {}
+	--print(xPlayer.identifier)
+	MySQL.Async.fetchAll("SELECT * FROM owned_vehicles WHERE owner = @identifier AND garage_name = @name", {['@identifier'] = xPlayer.identifier, ['@name'] = name}, --, state, garage_name, name, plate
+		function(results)
+
+			if results ~= nil and #results > 0 then
+				for i = 1, #results, 1 do
+					local this_data = {}
+					local vehName = results[i].name .. ' [' .. results[i].plate .. ']'
+					local state = results[i].state
+
+					if state then
+						this_data['value'] = results[i].vehicle
+					else
+						vehName = vehName .. ' (out)'
+						this_data['value'] = 'not_stored'
+					end
+					this_data['label'] = vehName
+					this_data['plate'] = results[i].plate
+					data[i] = this_data
+				end
+				TriggerClientEvent('yp_garage:openVehicleMenu', src, data)
+			else
+				TriggerClientEvent('mythic_notify:client:SendAlert', src, {type = 'error', text = 'You do not have any vehicles in this garage', length = 2500})
+			end
+		end)
+end)
+
+RegisterServerEvent('yp_garage:getAllVehicles')
+AddEventHandler('yp_garage:getAllVehicles', function(garageName)
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
+	local data = {}
+	MySQL.Async.fetchAll('SELECT state, garage_name, name, plate FROM owned_vehicles WHERE owner = @identifier', {['@identifier'] = xPlayer.identifier},
+		function(results)
+			if results ~= nil and #results > 0 then
+				for i = 1, #results, 1 do
+					local this_data = {}
+
+					--Format garage name
+					local garage = results[i].garage_name
+					garage = string.sub(garage, 1, string.find(garage, 'garage')-2)
+					--[[
+					Make garage name look prettier later.
+					for v = 1, #garage, 1 do
+						if garage[v] == '_' then
+							garage[v] = ' '
+						end
+					end
+					]]--
+
+					this_data['label'] = results[i].name .. ' [' .. results[i].plate .. '] '
+					if not results[i].state then
+						this_data['label'] = this_data['label'] .. '(out)'
+						this_data['value'] = 'out'
+					else
+						this_data['label'] = this_data['label'] .. '(' .. garage .. ')'
+						this_data['value'] = results[i].garage_name
+					end
+					this_data['plate'] = results[i].plate
+					data[i] = this_data
+				end
+				TriggerClientEvent('yp_garage:openInsureMenu', src, data, garageName)
+			else
+				TriggerClientEvent('mythic_notify:client:SendAlert', src, {type = 'inform', text = 'You do not own any vehicles.', length = 2500})
+			end
+		end)
+end)
+
+RegisterServerEvent('yp_garage:pullVehicle')
+AddEventHandler('yp_garage:pullVehicle', function(plateNum)
+	MySQL.Async.execute("UPDATE owned_vehicles SET state = false WHERE plate = @plate", {['@plate'] = plateNum},function() end)
+end)
+
+RegisterServerEvent('yp_garage:renameVehicle')
+AddEventHandler('yp_garage:renameVehicle', function(plate, name)
+	print(plate)
+	print(name)
+	MySQL.Async.execute('UPDATE owned_vehicles SET name = @name WHERE plate = @plate', {['@name'] = name, ['@plate'] = plate},function() end)
+end)
