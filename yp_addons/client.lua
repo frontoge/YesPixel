09466f21bb0bfc30169f99c48a8771607e464282
frontoge <coltsman1011@gmail.com>
2020-01-03 13:31:19 -0500

    added shuff commands

--- yp_addons/client.lua
+++ yp_addons/client.lua
@@ -99,6 +99,39 @@ AddEventHandler('yp_addons:addAmmo', function(ammoType)
 	AddAmmoToPed(ped, hash, 30)
 end)
 
+function moveSeat(seatNum)
+	local ped = GetPlayerPed(-1)
+	if IsPedInAnyVehicle(ped, false) then
+		local vehicle = GetVehiclePedIsIn(ped)
+		if GetPedInVehicleSeat(vehicle, seatNum-1) ~= ped then
+			if IsVehicleSeatFree(vehicle, seatNum-1) then
+				SetPedIntoVehicle(ped, vehicle, seatNum-1)
+			else
+				exports['mythic_notify']:DoHudText('error', 'Someone is alreay in this seat!')
+			end
+		else
+			exports['mythic_notify']:DoHudText('error', 'You are already in this seat')
+		end
+	end
+end
+
+RegisterCommand("shuff", function(source, args, raw) --change command here
+	moveSeat(0)
+    TriggerEvent("SeatShuffle")
+end, false) --False, allow everyone to run it
+
+RegisterCommand('shuff2', function(source, args)
+	moveSeat(1)
+end, false)
+
+RegisterCommand('shuff3', function(source, args)
+	moveSeat(2)
+end, false)
+
+RegisterCommand('shuff4', function(source, args)
+	moveSeat(3)
+end, false)
+
 Citizen.CreateThread(function()
 	while true do
 		local playerPed = GetPlayerPed(-1)
