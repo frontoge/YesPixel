1a57913db534ff0cd02af6677df3f0661051f260
frontoge <coltsman1011@gmail.com>
2020-01-03 13:35:36 -0500

    Added spikestrips

--- yp_spikestrips/client.lua
+++ yp_spikestrips/client.lua
@@ -1,9 +1,11 @@
 function popTire(vehicle, tireNum)
 	local num = math.random(1, 100)
-	if num >= 60 then
-		SetVehicleWheelHealth(vehicle, tireNum, 0.0)
+	if num >= 85 then
+		SetVehicleTyreBurst(vehicle, tireNum, true, 1000.0)
 	end
 end
+
+local bursting = false
 
 Citizen.CreateThread(function()
 	while true do
@@ -15,11 +17,16 @@ Citizen.CreateThread(function()
 				local strip = GetClosestObjectOfType(x, y, z, 3.0, GetHashKey('p_ld_stinger_s'))
 				if DoesEntityExist(strip) then
 					if IsEntityTouchingEntity(vehicle, strip) then
-						popTire(vehicle, 0)
-						popTire(vehicle, 1)
-						popTire(vehicle, 2)
-						popTire(vehicle, 3)
-
+						if not bursting then
+							for i = 0, 7, 1 do
+								if not IsVehicleTyreBurst(vehicle, i, true) then
+									popTire(vehicle, i)
+								end
+							end
+							bursting = true
+						end
+					elseif bursting then
+						bursting = false
 					end
 				end
 			end
